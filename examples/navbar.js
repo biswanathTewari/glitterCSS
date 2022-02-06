9cbf71e1757f5ba14c7ee9950598235ba515937e
Biswanath Tewari <biswanath@geekyants.com>
2022-02-06 12:33:04 +0530

    fix - responsiveness and navigation

--- examples/navbar.js
+++ examples/navbar.js
@@ -1,14 +1,22 @@
 const burger = document.querySelector(".burger");
 const navlinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const linkWrappers = document.querySelectorAll(".nav-links li");
+const links = document.querySelectorAll(".nav-link");
+const body = document.querySelector("body");
+const html = document.querySelector("html");
 
 const toggleNav = () => {
   navlinks.classList.toggle("navlinks-active");
-  links.forEach((link, index) => {
+  body.classList.toggle("body-overflow");
+  html.classList.toggle("body-overflow");
+  linkWrappers.forEach((link, index) => {
     link.style.animation = `nav-link-fade 0.5s ease forwards ${
-      index / links.length + 0.25
+      index / linkWrappers.length + 0.25
     }s`;
   });
 };
 
 burger.addEventListener("click", toggleNav);
+links.forEach((link) => {
+  link.addEventListener("click", toggleNav);
+});
